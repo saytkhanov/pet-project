c7a25106770affc26ce44d90ab5764afa59b6d38
saytkhanov <saytkhanov2014@mail.ru>
2023-02-05 16:51:02 +0300

    add settings for icons, svg and file loaders, create custom Button, ThemeSwitche e.t.c

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,11 +8,10 @@ import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme, ''])}>
-            <button onClick={toggleTheme}>-</button>
             <Navbar />
             <AppRouter/>
         </div>
