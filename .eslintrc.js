281ceb792d33c4029616a0533d38fab009893fb6
saytkhanov <saytkhanov2014@mail.ru>
2023-02-08 02:17:59 +0300

    stylelint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
       'plugin:react/recommended',
+      'airbnb',
+      'plugin:i18next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -17,6 +19,7 @@ module.exports = {
   plugins: [
       'react',
       '@typescript-eslint',
+      'i18next',
   ],
   rules: {
       'react/jsx-indent': [2, 4],
@@ -34,6 +37,7 @@ module.exports = {
       'import/extensions': 'off',
       'import/no-extraneous-dependencies': 'off',
       'no-underscore-dangle': 'off',
+      'i18next/no-literal-string': ['error', { markupOnly: true }],
   },
   globals: {
       __IS_DEV__: true,
